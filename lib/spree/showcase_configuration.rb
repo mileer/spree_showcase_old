fc5f969653072a0a607c4631541461d1d6b5d728
mileer <mileer>
2014-09-25 20:05:24 +0400

    New changes

--- lib/spree/showcase_configuration.rb
+++ lib/spree/showcase_configuration.rb
@@ -1,8 +1,8 @@
 
 class Spree::ShowcaseConfiguration < Spree::Preferences::Configuration
   preference :enabled, :boolean, default: true
-  preference :thumbnail_style, :string, default: '220x110#'
-  preference :showcase_style, :string, default: '940x470#'
+  preference :thumbnail_style, :string, default: '220x87#'
+  preference :showcase_style, :string, default: '700x276#'
   preference :number_of_slides_to_show, :integer, default: 4
   preference :slideshow, :boolean, default: true
   preference :animation_type, :string, default: 'fade'
